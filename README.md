61f77c17c9018684a5ed4bc6fbdaeea8eeaa5bba
okisanjp <okisan.jp@gmail.com>
2014-04-07 16:39:07 +0900

    addswwap : chmod 600 & $B%5%$%:$r%Q%i%a!<%?$K(B

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
  * copy file with datetime text ( YYMMDDHHSS )
 
 ### addswap
- * make swapfile (4GB)
+ * make swapfile
 
 ### install_zabbix_agentd_conf.sh
  * zabbix_agentd.conf作成用の簡易スクリプト
--- addswap/addswap.sh
+++ addswap/addswap.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-if [ ! -e /swap.ex ]; then
-    dd if=/dev/zero of=/swap.ex bs=1M count=4096
+if [ $1 ]; then
+  if [ ! -e /swap.ex ]; then
+    dd if=/dev/zero of=/swap.ex bs=1M count=$1
+    chmod 600 /swap.ex
     mkswap /swap.ex
     swapon /swap.ex
     echo "/swap.ex swap swap defaults 0 0" >> /etc/fstab
-else
+  else
     echo "/swap.ex already exists."
+  fi
+else
+  echo "Usage : sh ./addswap.sh SWAPSIZE(MB)"
 fi
